3666901767cb378b22c4ac191af32b8fa140e81c
Joseph Cosentino <jcosentino11@gmail.com>
2017-05-10 12:33:24 -0400

    Dirty context for documentation test

--- src/test/java/me/josephcosentino/ApiDocumentationTest.java
+++ src/test/java/me/josephcosentino/ApiDocumentationTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.restdocs.payload.FieldDescriptor;
